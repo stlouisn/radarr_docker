6ea26b06ff826fbfc6bc1954b7bdd76b6069ea28
stlouisn <stlouisn@hotmail.com>
2020-12-12 07:44:51 -0400

    Update get_version.sh

--- travis/get_version.sh
+++ travis/get_version.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 # Output radarr version from github:radarr releases
-echo "$(curl -fsSL --retry 5 --retry-delay 2 https://api.github.com/repos/Radarr/Radarr/releases | jq -r .['0'].tag_name | cut -c 2-)"
+echo "$(curl -fsSL --retry 5 --retry-delay 2 https://api.github.com/repos/Radarr/Radarr/releases | jq -r .['0'].tag_name | tr -d v)"
