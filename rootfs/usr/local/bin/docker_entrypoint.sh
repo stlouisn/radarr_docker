c70fba657711426cb30c73367d1a18964cde88db
stlouisn <stlouisn@hotmail.com>
2020-12-12 06:24:22 -0400

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -10,10 +10,12 @@ chown -R radarr:radarr /config
 
 #=========================================================================================
 
+# Workaround for ICU requirements - disable globalization
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+
 # Start radarr in console mode
 #exec gosu radarr \
 #    /usr/bin/mono --debug \
 #    /Radarr/Radarr.exe -nobrowser -data=/config
 exec gosu radarr \
-    DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1 \
     /Radarr/Radarr -nobrowser -data=/config
