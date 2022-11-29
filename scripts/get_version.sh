ea89a4268a311e85e69dd437cd62310b6fdaf0c8
stlouisn <stlouisn@hotmail.com>
2022-11-29 12:54:34 -0400

    Update get_version.sh

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # Application version
-APP_VERSION="$(curl -sSL --retry 5 --retry-delay 2 "https://radarr.servarr.com/v1/update/master/changes" | jq -r '.[0].version')"
+APP_VERSION="$(curl -sSL --retry 5 --retry-delay 2 "https://radarr.servarr.com/v1/update/master/changes" | jq -r '.[] | select ( .branch == "master" ) | .version' | sort -rn | head -n 1)"
 
 # Export C_VERSION
 echo "export C_VERSION=\""$APP_VERSION"\"" >> $BASH_ENV
